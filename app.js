04f1993fe2e0c5d4f56391fd27a45c095a637241
unknown <darkchinax@gmail.com>
2023-08-29 09:01:59 +0300

    4th

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const App = {
-    data() {
-        return {
-            title: "Counter",
-            counter: 0
-        }
+  data() {
+    return {
+      title: "Note list",
+      placeholder: "add note title...",
+      inputValue: "",
+      notes: ["note 01" , "note 02"]
+    };
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value;
+    },
+    addNote() {
+        this.notes.push(this.inputValue);
+        this.inputValue = "";
+    },
+    removeNote() {
+      this.notes.push(this.inputValue);
+  },
+    keyPressHandler(event) {
+      if(event.key === "Enter") {
+        this.addNote();
+      }
     }
-}
+  },
+};
 
 Vue.createApp(App).mount("#app");
